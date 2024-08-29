cbfd7288ccb83aa14d6937c6f622dba45ee623f2
Sparsh Shrivastava <141167991+sparshs07@users.noreply.github.com>
2024-08-29 20:07:05 +0530

    update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let type=new Typed('#changing-text',{
-    strings:['Front-End Developer','Api Designer','Java-Developer'],
+    strings:['Front-End Developer','Api Designer','Java Developer'],
     typeSpeed:50,
     backSpeed:50,
     loop:true
